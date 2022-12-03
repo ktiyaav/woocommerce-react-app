738f2b860e3fdb59c0901e5078bf3afb5e765c56
Shakti Saurav <28942894+7IG3R@users.noreply.github.com>
2022-12-04 01:11:42 +0530

    Woocommerce API

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,19 +1,26 @@
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
-import axios from 'axios';
-
-
-const instance = axios.create({
-  baseURL:  'https://9dna9.com/wp-json/wc/v1/',
-}); 
-
-instance.interceptors.request.use(config => {
-  config.params = {
-   consumer_key: 'ck_ae4e3193083fba843761745dca63594aa6bd741a',
-   consumer_secret: 'cs_03a2b3cdd5934456175c06d4b5b480e1ac3e1bb5',
-
-    ...config.params,
-  };
-  return config;
+const instance = new WooCommerceRestApi({
+  url: "https://jufy.shaktisaurav.com/",
+  consumerKey: "ck_e2bd3129c7dc1e1f2d10f80ee55771943d217fdb",
+  consumerSecret: "cs_e0c7143ec22e9bb1c9bfaf1c3efa75ffcd0b17f2",
+  version: "wc/v3",
+  queryStringAuth: true
 });
-
 export default instance; 
+
+
+// import axios from 'axios';
+// const instance = axios.create({
+//   baseURL:  'https://jufy.shaktisaurav.com/wp-json/wc/v1/',
+// }); 
+// instance.interceptors.request.use(config => {
+//   config.params = {
+//    consumer_key: 'ck_e2bd3129c7dc1e1f2d10f80ee55771943d217fdb',
+//    consumer_secret: 'cs_e0c7143ec22e9bb1c9bfaf1c3efa75ffcd0b17f2',
+
+//     ...config.params,
+//   };
+//   return config;
+// });
+// export default instance; 
