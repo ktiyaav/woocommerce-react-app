1ac2f3999e52d27e20d938b7ed55af64c2495272
Shakti Saurav <28942894+7IG3R@users.noreply.github.com>
2022-12-16 14:04:41 +0530

    Product Page

--- src/screens/Product.js
+++ src/screens/Product.js
@@ -21,69 +21,86 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 function Product(props) {
+
   const [add, setAdd] = useState(false);
   const {id} = useParams();
-  
   const [product, setProduct] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  const [src, setSrc] = useState(false);
 
-  console.log(props)
   useEffect(()=>{
     loadProduct();
+    
   },[]);
 
   const loadProduct = async () => {
       const products = await API.get('products/'+id);
       setProduct(products.data);
       console.log(products.data)
-      setSrc(products.data.images[0].src);
       setLoaded(true);
   }
+
+  const PINCheck = (e) => {
+    console.log(e.target.value)
+    // Check if PIN Code is from your deliverable area or not.
+  }
+
   return (
     <>
       <div className="product-page">
         {
-          (loaded == false)
-          ? <ProductLoader/>
-          : (<>
-          {
-            (add == false)
-            ? (
-              <Navbar title={product.name} description={'Store: ' + product.store.store_name} path={'/search'}/>
-              )
-            :(
-              <div className="product-added">
-              <div className="cart-back-arrow">
-                <ArrowLeft/>
+          (loaded)
+          ? <>
+            <Navbar title={product.name} description={'Store: ' + product.store.store_name} path={'/search'}/>
+
+            <img src={product.images[0].src} alt={product.name} className="below-header-bar single-product-image" />
+
+            <div className="p-medium">
+              <div className="">
+                <h3 className="title">{product.name}</h3>
+                <h3 className="product-price">₹{product.price}</h3> <h3 className="product-regular-price"> MRP ₹{product.regular_price}</h3>
               </div>
-              <div className="product-title">
-              {product.name} has been added to cart
-              </div></div>
-              )
-          } 
-            <img src={src} className="single-product-image" />
-            <div className="container">
-              <br />
-              <h3 className="product-price">₹{product.price}</h3>
-              <p>{product.short_description}</p>
-              <p>Category: {product.categories[0].name}</p>
             </div>
-            <div className="row bott-product-button">
-            <div className="col-4 px-0">
-              <button onClick={()=> {props.addtoCart(product); setAdd(true);}} className="cart-button">Add to cart</button>
+
+            <div className="hr-break"></div>
+
+            <div className="p-medium">
+              <div className="widget-title">Select Size</div>
+              <form className="delivery-check">
+                  <select></select>
+                </form>
+                <p className="fw-light">Select which size you want!</p>
             </div>
-            <div className="col-8 px-0">
-              <button className="buy-button">Buy Now</button>
+
+            <div className="hr-break"></div>
+
+            <div className="p-medium">
+            <div className="widget-title">Shipping & Delivery</div>
+              <form className="delivery-check">
+                  <input type="text" placeholder="Enter your PIN code" name="pincode" onChange={PINCheck}></input>
+                </form>
+                <p className="fw-light">Enter your pincode to check if {product.name} can be delivered to your area.</p>
             </div>
+            
+            <div className="hr-break"></div>
+
+            <div className="p-medium">
+            <div className="widget-title">Product Details</div>
+              <span className="title">Categories : </span> {product.categories[0].name}
+              <p>{product.short_description.replace(/<\/?[^>]+>/gi, '')}</p>
+              {product.store.store_name ? <span className="title">Store: {product.store.store_name}</span> : null}
+            </div>
+
+            <div className="space-large"></div>
+
+            <div className="row bott-product-button d-flex justify-content-center align-content-center">
+                <button onClick={()=> {props.addtoCart(product); setAdd(true);}} className="cart-button">Add to cart</button>
+                <button className="buy-button">Buy Now</button>
             </div>
             </>
-          )
+          :<ProductLoader/>
         }
       </div>
     </>
   );
 }
-
-
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
